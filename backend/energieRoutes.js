e7554370ec42b309ec3845d6a3d699eeafcad2fc
matthijsgv <mattyyoyo@live.com>
2024-02-29 13:17:46 +0100

    new folder structure

--- backend/energieRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const energieController = require("./controller/energieController");
-const router = express.Router();
-
-router.post("/energie-meting", energieController.saveEnergieMeting);
-router.get("/last-energie-meting", energieController.getLastEnergieMeting);
-
-module.exports = router;
